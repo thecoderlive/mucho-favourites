b1426ef7535a5f60fe2434962c95bee773645c9d
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-01-23 10:39:46 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,45 @@
+export const item = {
+    "id": "3fb4ddeb-eba5-401f-a683-d29bc4a44332",
+    "favourite_list": [
+        {
+            "id": "c64bc99c-59f2-4771-b5fb-f333c5418dfc",
+            "food_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/32.png",
+            "food_name": "contain",
+            "variety_name": "school",
+            "made_by": "dog",
+            "heart_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/211.png"
+        },
+        {
+            "id": "4517701d-7bb2-4121-aa24-d1979da4566f",
+            "food_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/198.png",
+            "food_name": "degree",
+            "variety_name": "artist",
+            "made_by": "much",
+            "heart_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/3.png"
+        },
+        {
+            "id": "85add79a-a618-4976-aa0d-6d47a5b1f877",
+            "food_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/349.png",
+            "food_name": "for",
+            "variety_name": "mother",
+            "made_by": "seven",
+            "heart_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/91.png"
+        },
+        {
+            "id": "d67c3386-10a5-4315-bf3c-54e3f182aa5e",
+            "food_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/16.png",
+            "food_name": "yes",
+            "variety_name": "kind",
+            "made_by": "add",
+            "heart_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/206.png"
+        },
+        {
+            "id": "58258583-4a73-4a60-a119-edf17a2dd8a0",
+            "food_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/102.png",
+            "food_name": "suddenly",
+            "variety_name": "some",
+            "made_by": "common",
+            "heart_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/133.png"
+        }
+    ]
+}
